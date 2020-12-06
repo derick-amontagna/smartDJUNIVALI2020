40101d96c81b11dcd685d26ae0dfbed0a738453c
Derick <doxdank@hotmail.com>
2020-12-06 18:57:49 -0300

    Ajeitando o sensor de corrente - Versão 1.0

--- main/main.ino
+++ main/main.ino
@@ -25,13 +25,15 @@ int rele2Comando;
 #define sensor1 32
 #define sensor2 33
 #define rele1 26
-#define rele2 13
+#define rele2 25
 
 // Do algoritmo
 // Sensor
-int mVperAmp = 66; // use 185 para 5A ou 100 para 20A ou 66 para 30A - Sensibilidade do sensor
+int mVperAmp = 100; // use 185 para 5A ou 100 para 20A ou 66 para 30A - Sensibilidade do sensor
 int Namostras = 150; // Numero de amostras a serem somadas para se obter a leitura final
-float offsetCorrente = 2.5; // tensão de saída quando a corrente é zero -> vcc × 0.5 (2.5 v)
+float VCC = 4.82; // tensão de saída quando a corrente é zero -> vcc × 0.5 (2.5 v)
+float offsetCorrente = VCC*0.5; // tensão de saída quando a corrente é zero 
+
 // Conta de luz
 float potenciaAtivaW[2] = {};  // vetor com as potencias ativa
 float energiaKWH[2] = {};  // QuiloWatt-hora gastos 
@@ -74,26 +76,38 @@ BLYNK_WRITE(V1) {
 
 /// Funções
 void leituraACS712(){
-  float amostras1 = 0.0, valorLido1 = 0.0, valorMedio1 = 0.0, tensao1 = 0.0, corrente1 = 0.0;
-  float amostras2 = 0.0, valorLido2 = 0.0, valorMedio2 = 0.0, tensao2 = 0.0, corrente2 = 0.0;
+  float amostras1 = 0.0, valorLido1 = 0.0, valorMedio1 = 0.0, valorLidoFinal1 = 0.0, corrente1 = 0.0;
+  float amostras2 = 0.0, valorLido2 = 0.0, valorMedio2 = 0.0, valorLidoFinal2 = 0.0, corrente2 = 0.0;
   
   for (int x = 0; x < Namostras; x++){
     valorLido1 = analogRead(sensor1);
+    Serial.println(valorLido1);
     valorLido2 = analogRead(sensor2);
     amostras1 = amostras1 + valorLido1;
     amostras2 = amostras2 + valorLido2;
+    
   }
   valorMedio1 = amostras1/Namostras;
   valorMedio2 = amostras2/Namostras;
   
-  tensao1 = ((valorMedio1 * 5/1024.0));
-  tensao2 = ((valorMedio2 * 5/1024.0));
+  valorLidoFinal1 = ((valorMedio1 * (VCC/4096.0)));
+  valorLidoFinal2 = ((valorMedio2 * (VCC/4096.0)));
   
-  corrente1 = ((tensao1 - offsetCorrente)/ mVperAmp);
-  corrente2 = ((tensao2 - offsetCorrente)/ mVperAmp);
+  corrente1 = (valorLidoFinal1 - offsetCorrente)*1000;
+  corrente1 = (corrente1/ mVperAmp);
+  corrente1 = (corrente1/ 1.41421356);
+  corrente2 = (valorLidoFinal2 - offsetCorrente)*1000;
+  corrente2 = (corrente2/ mVperAmp);
+  corrente2 = (corrente2/ 1.41421356);
+
+
+ // Serial.print("Valor do 1 da corrente");
+ // Serial.println(corrente1);
+  //Serial.print("Valor do 2 da corrente");
+  //Serial.println(corrente2);
   
-  potenciaAtivaW[0] = potenciaAtivaW[0] + tensao1*corrente1; 
-  potenciaAtivaW[1] = potenciaAtivaW[1] + tensao2*corrente2; 
+  potenciaAtivaW[0] = potenciaAtivaW[0] + VCC*corrente1; 
+  potenciaAtivaW[1] = potenciaAtivaW[1] + VCC*corrente2; 
 }
 
 void calcEnergiaKWH(){
@@ -123,8 +137,12 @@ void enviandoContaFinal(){
   Blynk.virtualWrite(V5, valorFinalConta);
   valorAtualConta = 0;
   valorFinalConta = 0;
+  energiaTotal = 0;
   energiaKWH[0] = 0;
   energiaKWH[1] = 0; 
+  Blynk.virtualWrite(V2, valorAtualConta);
+  Blynk.virtualWrite(V3, energiaTotal);
+  Blynk.virtualWrite(V4, valorAtualConta);
 }
 
 
@@ -138,11 +156,12 @@ void setup() {
   pinMode(rele1, OUTPUT);
   pinMode(rele2, OUTPUT);
 
-  timer.setInterval(3600000L, calcEnergiaKWH);
-  timer.setInterval(2628000000L, enviandoContaFinal);
+  timer.setInterval(60000L, calcEnergiaKWH);   // 1 mins em milisegundos 
+  timer.setInterval(180000L, enviandoContaFinal);   // 3 mins em milisegundos
 }
 
 void loop() {
   Blynk.run(); // Para o App se comunicar a todo instante
   timer.run();
+  leituraACS712();
 }
